ddfd8423861022ec810a14631eaf7152b4ee7779
Amr-Abdelrzek <amr.3brazek#@gmail.com>
2023-02-17 13:25:23 -0800

    fifth

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%i", n);
+	}
+	putchar('\n');
+	return (0);
+}
