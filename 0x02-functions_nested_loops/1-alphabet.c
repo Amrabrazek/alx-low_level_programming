89b7c647e694157bdb56c6652b4dc2defc3fd9de
Amr-Abdelrzek <amr.3brazek#@gmail.com>
2023-02-21 13:13:57 -0800

    1st

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * print the alphabet - make 
+ * print the alphabet - make
  * Return: void
  */
 void print_alphabet(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
