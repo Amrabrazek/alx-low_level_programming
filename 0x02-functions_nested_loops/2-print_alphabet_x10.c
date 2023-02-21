88733466cd6e4866d1e36a078c612f88259b221b
Amr-Abdelrzek <amr.3brazek#@gmail.com>
2023-02-21 14:20:59 -0800

    2nd

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,21 @@
 #include "main.h"
 
 /**
+ * print_alphabet - make
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
+
+/**
  * print_alphabet_x10 - make
  * Return: void
  */
