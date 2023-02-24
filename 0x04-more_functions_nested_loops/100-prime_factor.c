86c02482ae2e259d54f10f82d262abe49fd6a3cb
Amr-Abdelrzek <amr.3brazek#@gmail.com>
2023-02-24 14:22:41 -0800

    100th

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -4,18 +4,20 @@
  * Return: 0 if upper
  */
 
-int main(int c)
+int main(void)
 {
-	int x;
-	int ca;
+	long int x;
+	long int ca;
+	long int c;
 
-	for (x = 1; x <= c; x++)
+	c = 612852475143;
+	for (x = 1; x <= (c - 1); x++)
 	{
 		if ((c % x) == 0)
 		{
 			ca = x;
 		}
 	}
-	printf("%i\n", c)
+	printf("%ld\n", ca);
 	return (0);
 }
