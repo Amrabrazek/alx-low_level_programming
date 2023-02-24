92c7b263516327957c58b42ee60c35c3ca28ba25
Amr-Abdelrzek <amr.3brazek#@gmail.com>
2023-02-24 12:01:07 -0800

    8th

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include"main.h"
+/**
+ * print_square - check
+ * @n: number of rows
+ * Return: 0 if upper
+ */
+
+void print_square(int n)
+{
+	int c;
+	int x;
+
+	for (c = 0; c < n; c++)
+	{
+		for (x = 0; x < n; x++)
+		{
+			_putchar('#');
+		}
+	_putchar('\n');
+	}
+
+}
