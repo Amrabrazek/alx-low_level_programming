94fed5eaf7b607265cb2b11f3426bd5b99ea40b5
Amr-Abdelrzek <amr.3brazek#@gmail.com>
2023-02-24 13:18:29 -0800

    9th

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,4 @@
-#include"main.h"
+#include"stdio.h"
 /**
  * main - check
  * Return: 0 if upper
@@ -7,55 +7,31 @@
 int main(void)
 {
 	int c;
-	int x;
 
 	for (c = 1; c <= 100; c++)
 	{
 		if ((c % 3) == 0 && (c % 5) == 0)
 		{
-			_putchar('F');
-			_putchar('i');
-			_putchar('z');
-			_putchar('z');
-			_putchar('B');
-			_putchar('u');
-			_putchar('z');
-			_putchar('z');
-			_putchar(' ');
+			printf("FizzBuzz ");
 		}
 		else if ((c % 3) == 0)
 		{
-			_putchar('F');
-			_putchar('i');
-			_putchar('z');
-			_putchar('z');
-			_putchar(' ');
+			printf("Fizz ");
 		}
 		else if ((c % 5) == 0)
 		{
-			_putchar('B');
-			_putchar('u');
-			_putchar('z');
-			_putchar('z');
+			printf("Buzz");
+	
 			if (c != 100)
 			{
-				_putchar(' ');
+				putchar(' ');
 			}
 		}
 		else
 		{
-			if (c > 9)
-			{
-				_putchar((c / 10) + '0');
-				_putchar((c % 10) + '0');
-			}
-			else
-			{
-				_putchar(c + '0');
-			}
-
-			_putchar(' ');
+			printf("%i ", c);
 		}
 	}
-	_putchar('\n');
+	putchar('\n');
+	return (0);
 }
