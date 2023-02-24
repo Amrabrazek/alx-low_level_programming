eb35826ef4c4851ad8c6253b2ec366a61e330b1b
Amr-Abdelrzek <amr.3brazek#@gmail.com>
2023-02-24 07:43:53 -0800

    5th

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include"main.h"
+/**
+ * more_numbers- check
+ * Return: 0 if upper
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int c = 10;
+
+	while (c--)
+	{
+	for (i = 0; i <= 14; i++)
+	{
+		if (i >= 10)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		else
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+	}
+}
